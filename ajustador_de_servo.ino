2d3cab6b0066c31bd7bee187dab08555147f09d8
CREATVRA <creatvra.duvan@gmail.com>
2018-02-09 11:21:10 -0500

    Add files via upload

---
+++ ajustador_de_servo.ino
@@ -0,0 +1,16 @@
+ // incluimos la libreria del servomotor
+ #include <Servo.h>
+
+   // Le indicamos a la libreria que vamos a usar
+  // un solo servomotor llamado "ServoMotor"
+Servo ServoMotor;
+
+void setup() {
+  
+    ServoMotor.attach(6);
+}
+
+void loop() {
+  ServoMotor.write(180); // Ajustamos el servo a 180 grados
+
+}
